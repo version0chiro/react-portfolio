66161e150f7bb5a120ea33fd7d4f066677049bc6
Chiro <sachinsinghroxx@gmail.com>
2021-05-30 17:18:23 +0530

    Updates

---
+++ precache-manifest.4e7118f63dee967f91c766cd46e316b7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b48aef6b9a9ed72fca88571c7725ba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "21079bf9e5bb7d609374",
+    "url": "/portfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "efd8cbe3510b1a604e34",
+    "url": "/portfolio/static/css/main.1a28949a.chunk.css"
+  },
+  {
+    "revision": "21079bf9e5bb7d609374",
+    "url": "/portfolio/static/js/2.fb2aab2b.chunk.js"
+  },
+  {
+    "revision": "3ce2d0825dbb8942398140ee15e488fe",
+    "url": "/portfolio/static/js/2.fb2aab2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efd8cbe3510b1a604e34",
+    "url": "/portfolio/static/js/main.72870c76.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "1a942e1f0367482fad90fe695f89e2fb",
+    "url": "/portfolio/static/media/about.1a942e1f.png"
+  },
+  {
+    "revision": "c6f794fb8104a658a046088d2395a82d",
+    "url": "/portfolio/static/media/about1.c6f794fb.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/portfolio/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/portfolio/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/portfolio/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/portfolio/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/portfolio/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/portfolio/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/portfolio/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/portfolio/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "dbdb2277066a46a7e5266f231de95a77",
+    "url": "/portfolio/static/media/male.dbdb2277.png"
+  },
+  {
+    "revision": "d995a6c1bd54be0a99d4558dac049c89",
+    "url": "/portfolio/static/media/male1.d995a6c1.png"
+  }
+]);
