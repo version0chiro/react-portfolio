698390694187de6a14d567766ecf5e0dfe37b5b4
Chiro <sachinsinghroxx@gmail.com>
2021-05-30 17:05:48 +0530

    completed techStack

--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -7,15 +7,33 @@ import {
   DiNodejs,
   DiMongodb,
   DiPython,
+  DiHtml5,
+  DiJava,
+  DiCss3,
   DiGit,
+  DiSqllite,
 } from "react-icons/di";
-import { SiPytorch, SiTensorflow, SiFirebase } from "react-icons/si";
+import {
+  SiPytorch,
+  SiTensorflow,
+  SiAmazonaws,
+  SiFlask,
+  SiMysql,
+  SiRaspberrypi,
+  SiAndroid,
+  SiArduino,
+  SiNativescript,
+  SiKeras,
+  SiC,
+  SiSvelte,
+  SiTypescript,
+} from "react-icons/si";
 
 function Techstack() {
   return (
     <Container fluid className="about-section">
       <h1 className="project-heading">
-        <strong className="color-a">Techstack</strong>
+        <strong className="color-a">Languages</strong>
       </h1>
       <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
         <Col xs={4} md={2} className="tech-icons">
@@ -25,28 +43,71 @@ function Techstack() {
           <DiJavascript1 />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
-          <DiNodejs />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiReact />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiMongodb />
+          <SiTypescript />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
           <DiPython />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
-          <SiPytorch />
+          <DiJava />
         </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiHtml5 />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiCss3 />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiC />
+        </Col>
+      </Row>
+      <h1 className="project-heading">
+        <strong className="color-a">Framework/Technologies</strong>
+      </h1>
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
         <Col xs={4} md={2} className="tech-icons">
           <SiTensorflow />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
-          <DiGit />
+          <DiReact />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
-          <SiFirebase />
+          <DiNodejs />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiPytorch />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiSvelte />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiAmazonaws />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiFlask />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiKeras />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiNativescript />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiAndroid />
+        </Col>
+      </Row>
+      <h1 className="project-heading">
+        <strong className="color-a">Databases</strong>
+      </h1>
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiMongodb />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiSqllite />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <SiMysql />
         </Col>
       </Row>
     </Container>
